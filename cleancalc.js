088ac7a7899b9a8863528eb5ea127ea13c393dc0
Javier <Javier.gonzalez@eliuma.academygit config --global user.email youremail@domain.comgit config --global user.email javier.gonzalez@elium.academy>
2017-09-25 13:56:28 +0200

    Second Version

--- cleancalc.js
+++ cleancalc.js
@@ -19,3 +19,12 @@ function divide(arg1, arg2) {
 lastResult = add(2, 4);
 lastResult = add(5, lastResult);
 lastResult = multiply(3,2);
+lastResult = 0000;
+
+function operateIntermediary(operation, arg1, arg2) {
+	return operation(arg1, arg2);
+};
+
+lastResult = operateIntermediary(add, 2, 4);
+lastResult = operateIntermediary(add, 5, lastResult);
+lastResult = operateIntermediary(multiply, 3,2);
